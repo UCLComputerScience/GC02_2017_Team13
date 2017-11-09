16b15721e2009d7aafdaa9f49540de721be07abd
Lucio D'Alessandro <Lucio@eduroam-int-dhcp-97-108-242.ucl.ac.uk>
2017-11-09 17:00:30 +0000

    lalal

--- Ishango/src/pages/stock/stock.ts
+++ Ishango/src/pages/stock/stock.ts
@@ -15,6 +15,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class StockPage {
 
+  public reorderIsEnabled = true;
+
+
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
