7f3f27fdfcb41567419573776312fb27bda1f8cd
Dimitris Selalmazidis <ucabela@ucl.ac.uk>
2017-11-09 13:34:20 +0000

    first

---
+++ src/pages/photosound/photosound.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { PhotosoundPage } from './photosound';
+
+@NgModule({
+  declarations: [
+    PhotosoundPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(PhotosoundPage),
+  ],
+})
+export class PhotosoundPageModule {}
