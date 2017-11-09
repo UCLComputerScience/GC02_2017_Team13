a782ec8cafd42143e7739e99a9808bf42809369c
Dimitris Selalmazidis <ucabela@ucl.ac.uk>
2017-11-09 15:06:29 +0000

    please

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,61 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { ProductsPage } from '../pages/products/products';
-import { SalesPage } from '../pages/sales/sales';
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
-import { RunningcPage } from '../pages/runningc/runningc';
-import { StockPage } from '../pages/stock/stock';
-import { FeaturesPage } from '../pages/features/features';
-import { PhotosoundPage } from '../pages/photosound/photosound';
-import { Camera, CameraOptions } from '@ionic-native/camera';
-import { QuantityPage } from '../pages/quantity/quantity';
-import { IscashpaidPage } from '../pages/iscashpaid/iscashpaid';
+
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { QuantityProvider } from '../providers/quantity/quantity';
-import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
     MyApp,
-    ProductsPage,
-    SalesPage,
+    AboutPage,
+    ContactPage,
     HomePage,
-    TabsPage,
-    RunningcPage,
-    StockPage,
-    PhotosoundPage,
-    FeaturesPage,
-    QuantityPage,
-    IscashpaidPage,
-
+    TabsPage
   ],
   imports: [
-    BrowserModule,HttpModule,
-    IonicModule.forRoot(MyApp,{tabsHideOnSubPages:true})
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    ProductsPage,
-    SalesPage,
+    AboutPage,
+    ContactPage,
     HomePage,
-    TabsPage,
-    RunningcPage,
-    StockPage,
-    PhotosoundPage,
-    FeaturesPage,
-    QuantityPage,
-    IscashpaidPage,
-    
+    TabsPage
   ],
   providers: [
     StatusBar,
-    SplashScreen,Camera,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
-    QuantityProvider
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
 export class AppModule {}
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,26 +1,17 @@
 import { Component } from '@angular/core';
 
-import { ProductsPage } from '../products/products';
-import { SalesPage } from '../sales/sales';
+import { AboutPage } from '../about/about';
+import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
-import { RunningcPage } from '../runningc/runningc';
-import { StockPage } from '../stock/stock';
-import { FeaturesPage } from '../features/features';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
-  public hideTabs:boolean = true;
 
   tab1Root = HomePage;
-  tab2Root = ProductsPage;
-  tab3Root = SalesPage;
-  tab4Root = RunningcPage;
-  tab5Root = StockPage;
-  tab6Root = FeaturesPage;
-
-
+  tab2Root = AboutPage;
+  tab3Root = ContactPage;
 
   constructor() {
 
